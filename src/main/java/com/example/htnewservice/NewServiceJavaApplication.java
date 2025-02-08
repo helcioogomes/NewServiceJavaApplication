988614233a1b5b94ce4101d74ed589a7fc1b7fa4
Helcio <helciot@hotmail.com>
2025-02-08 16:44:07 -0300

    Setting a Service in Java with SpringBoot

---
+++ src/main/java/com/example/htnewservice/NewServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.htnewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewServiceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewServiceJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/htnewservice/HtnewserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htnewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtnewserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
